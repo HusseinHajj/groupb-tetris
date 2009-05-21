3a04decf9006c7f01b77a1ff2092933e60c0260c
ddolphinwave77 <ddolphinwave77@a5ada6b6-448f-11de-9a54-b9394989f659>
2009-05-21 17:45:42 +0000

    Shapes now drop and rotate

--- trunk/Tetris/TetrisWindow.xaml.cs
+++ trunk/Tetris/TetrisWindow.xaml.cs
@@ -41,81 +41,169 @@ namespace Tetris
 			InitializeComponent();
 			gameTimer.Interval = TimeSpan.FromMilliseconds(50);
 			gameTimer.Tick += new EventHandler(gameTimer_Tick);
-            OShape o = new OShape();
-            Board.Children.Add(o);
+            gameTimer.Start();
 		}
 
-		public Shape GetCurrentShape()
+        UIElement nextShape = new UIElement();
+        Random generator = new Random();
+        bool nextShapeExists = false;
+
+
+		public UIElement GetCurrentShape()
 		{
-			return Board.Children.OfType<Shape>().ElementAt(Board.Children.Count - 1);
+			return Board.Children[Board.Children.Count - 1];
 		}
 
-		public List<Shape> GetStackedShapes()
-		{
-			Shape currentShape = GetCurrentShape();
-			return Board.Children.OfType<Shape>().Where(shape => shape != currentShape).ToList();
-		}
+        //public List<Shape> GetStackedShapes()
+        //{
+        //    Shape currentShape = GetCurrentShape();
+        //    return Board.Children.OfType<Shape>().Where(shape => shape != currentShape).ToList();
+        //}
+
+        UIElement shapeToAdd;
+        int currentY = 0;
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
             if (!activePiece)
             {
-
+                if (!nextShapeExists)
+                {
+                    GenerateNewShape();
+                    nextShapeExists = true;
+                }
+                Grid1.Children.Remove(nextShape);
+                shapeToAdd = nextShape;
+                Canvas.SetLeft(shapeToAdd, 100);
+                Board.Children.Add(shapeToAdd);
+                GenerateNewShape();
+                activePiece = true;
             }
+            if (activePiece)
+            {
+                if (currentY + 80 <= 400)
+                {
+                    currentY += 5;
+                    Canvas.SetTop(Board.Children[Board.Children.Count - 1], currentY);
+                }
+                else
+                {
+                    currentY = 0;
+                    activePiece = false;
+                }
+            }
+
+
+
+            /*pseudocode for boundry checking. can be moved anywhere
+             * On left/right click += LeftRightEvent
+             * LeftRightevent()
+             * {
+             *      onLeftClick
+             *      if(leftClickResult.location.X < boardBoarder.Location.leftX)
+             *      {
+             *          don't allow move
+             *      }
+             *      onRightClick
+             *      if(rightClickResult.Location.X > boardBoarder.Location.rightX)
+             *      {
+             *          don't allow move
+             *      }
+             * }
+             * On rotation += RotationEvent
+             * RotationEvent()
+             * {
+             *      if(rotationResult.location.X < boardBoarder.Location.leftX)
+             *      {
+             *          item.location = item.location + (boardBoarder.Location.leftX - rotationResult.location.X)
+             *      }
+             *      else if(rotationResult.location.X > board.Boarder.Location.rightX)
+             *      {
+             *          item.location = item.location - (rotationResult.location.X - boardBoarder.Location.rightX)
+             *      }
+             * }
+            */
+        }
+
+        private void GenerateNewShape()
+        {
+            switch (generator.Next() % 7)
+            {
+                case 0:
+                    IShape newShape = new IShape();
+                    nextShape = newShape;
+                    break;
+                case 1:
+                    JShape newShape1 = new JShape();
+                    nextShape = newShape1;
+                    break;
+                case 2:
+                    LShape newShape2 = new LShape();
+                    nextShape = newShape2;
+                    break;
+                case 3:
+                    OShape newShape3 = new OShape();
+                    nextShape = newShape3;
+                    break;
+                case 4:
+                    ZShape newShape4 = new ZShape();
+                    nextShape = newShape4;
+                    break;
+                case 5:
+                    S_Shape newShape5 = new S_Shape();
+                    nextShape = newShape5;
+                    break;
+                case 6:
+                    TShape newShape6 = new TShape();
+                    nextShape = newShape6;
+                    break;
+            }
+            Grid.SetRow(nextShape, 1);
+            Grid1.Children.Add(nextShape);
         }
 
         private void Board_KeyDown(object sender, KeyEventArgs e)
         {
-            double left = Canvas.GetLeft(Board);
-            double right = Canvas.GetRight(Board);
-
-            int child = Board.Children.Count - 1;
-            double pieceLeft = Canvas.GetLeft(Board.Children[child]);
-            double pieceRight = Canvas.GetRight(Board.Children[child]);
-
             switch (e.Key)
-            {//Board.Children[Board.Children.Count - 1]
+            {
                 case Key.Up:
-                    //add rotation before the check
-
-                    pieceLeft = Canvas.GetLeft(Board.Children[child]);
-                    pieceRight = Canvas.GetRight(Board.Children[child]);
-                    if (pieceLeft < left)
-                    { 
-                        Canvas.SetLeft(Board.Children[child], left);
-                    }
-                    else if (pieceRight > right)
-                    {
-                        Canvas.SetRight(Board.Children[child], right);
-                    }
+                    RotateCurrentShape();
                     break;
                 case Key.Right:
-                    if (pieceRight == right)
-                    {
-                        //piece will not move right
-                    }
-                    else
-                    { 
-                        //add code to move the piece right here
-                        Canvas.SetRight(Board.Children[child], pieceRight + 20);
-                    }
+                    //if (pieceRight == right)
+                    //{
+                    //    //piece will not move right
+                    //}
+                    //else
+                    //{ 
+                    //    //add code to move the piece right here
+                    //    Canvas.SetRight(Board.Children[child], pieceRight + 20);
+                    //}
                     break;
                 case Key.Left:
-                    if (pieceLeft == left)
-                    {
-                        //piece will not move left
-                    }
-                    else
-                    { 
-                        //add code to move the piece left here
-                        Canvas.SetLeft(Board.Children[child], pieceLeft - 20);
-                    }
+                    //if (pieceLeft == left)
+                    //{
+                    //    //piece will not move left
+                    //}
+                    //else
+                    //{ 
+                    //    //add code to move the piece left here
+                    //    Canvas.SetLeft(Board.Children[child], pieceLeft - 20);
+                    //}
                     break;
                 case Key.Down:
                     break;
                 case Key.P:
                     break;
             }
+        }
+
+        int currentTransform = 0;
+
+        private void RotateCurrentShape()
+        {
+            currentTransform += 90;
+            Board.Children[Board.Children.Count - 1].RenderTransform = new RotateTransform(currentTransform, 30, 20);
         }
 	}
 }
